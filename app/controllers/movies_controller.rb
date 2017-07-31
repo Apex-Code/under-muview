9beac5847c5e571389e3876bb7fe973f99c0df15
Apex-Code <upstaterounder@gmail.com>
2017-07-30 22:12:59 -0500

    add notices to update, create, and destroy methods

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,7 +14,7 @@ class MoviesController < ApplicationController
   def create
   	@movie = Movie.create(movie_params)
   	if @movie.save
-  	  redirect_to movie_path(@movie)
+  	  redirect_to @movie, notice: "Your addition is appreciated!"
   	else
   	  render 'new'
   	end
@@ -25,12 +25,14 @@ class MoviesController < ApplicationController
 
   def update
   	if @movie.update
-      redirect_to movie_path(@movie)
+      redirect_to @movie, notice: "Your update was successful!"
     else
       render 'edit'	
     end
   end
 
   def destroy
+  	@movie.destroy notice
+  	  redirect_to root_path, notice: "It's as if this movie never existed!"
   end
 end
