7eb3319e599155638f1893066216acddce875d68
Apex-Code <upstaterounder@gmail.com>
2017-07-31 10:16:08 -0500

    create review model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
+  has_many :reviews	
   has_many :movies
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
   attr_accessor :login
 
 end
