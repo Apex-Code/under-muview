3efebe31c58aa8048fff79ea14bced49b612311b
Apex-Code <upstaterounder@gmail.com>
2017-07-30 22:06:51 -0500

    start stubbing out movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,36 @@
 class MoviesController < ApplicationController
+
+  def index
+  	@movies = Movie.all.order("created_at DESC") 
+  end
+  
+  def show
+  end
+
+  def new
+  	@movie = Movie.new
+  end
+
+  def create
+  	@movie = Movie.create(movie_params)
+  	if @movie.save
+  	  redirect_to movie_path(@movie)
+  	else
+  	  render 'new'
+  	end
+  end
+
+  def edit
+  end
+
+  def update
+  	if @movie.update
+      redirect_to movie_path(@movie)
+    else
+      render 'edit'	
+    end
+  end
+
+  def destroy
+  end
 end
